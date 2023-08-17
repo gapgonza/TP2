84a43b47e9e5450a10843391a9a5bc54644bf603
Alan Gonzalo Peñiñorey <gapgonza@gmail.com>
2023-08-17 11:45:14 -0300

    Netbeans TP2

---
+++ TrabajoPractico2/src/trabajopractico2/TrabajoPractico2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabajopractico2;
+
+/**
+ *
+ * @author Gonza
+ */
+public class TrabajoPractico2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Prueba de fetch,pull - push
+    }
+    
+}
